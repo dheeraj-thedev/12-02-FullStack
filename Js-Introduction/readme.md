31a5d8f8745cf56bc69627f41644176f5668899c
Dheeraj Singh <bond.deejay@gmail.com>
2021-05-29 13:28:02 +0530

    Added initial code

---
+++ Js-Introduction/readme.md
@@ -0,0 +1,32 @@
+
+## what is this .md it is actually a rakdown file ?
+    JavaScript Interpreter/Runtime
+
+    Q) What is compilaytion vs interpretion
+        Browser | Local -> Nodes JS that internally used chorm's V8 
+
+    Dynamic Type
+
+    var x = 10
+    x="My Name"
+
+    Statically Typed
+
+    int z =100
+        
+
+    6 Data Types
+    variable values and their types
+    how form an instance yoy may call refrence type 
+        1.Value 
+        2 Refrence 
+    What is Function ?
+
+    We do some hands on to our browser -> We will manipulate the DOM 
+        What is DOM ? 
+
+
+
+Internet World => Chrome IE Fir 
+    curl that is used to make HTTTP calls from your shell and command prompt
+    ie
---
+++ Js-Introduction/test.js
@@ -0,0 +1,54 @@
+
+
+
+
+HTML ->
+
+Ask for the details from a student 
+    Name 
+    age
+    contactNumber
+    email Address
+
+student{
+    name:value,
+    age:value,
+    constNumbr:value
+    email:value
+}
+
+
+<table></table>
+
+
+
+
+
+// for(var i =0; i<10; i++){
+//     console.log(i)
+// }
+
+// console.log("The value of i is :",i)
+
+//Hoisitng
+// What is i, $, p, and q afterwards?
+
+// var i = -1;
+
+// for (var i = 0; i < 10; i += 1) 
+// {
+//     var $ = -i;
+// }
+// if (true) {
+//     let p = 'FOO';
+//     console.log(p)
+// }
+//  else {
+//     var q = 'BAR';
+// }
+
+// console.log("I",i)
+// console.log("Dollar",$)
+// console.log("Queee",q)
+//console.log("P",p)
+// Check the next slide for an answer...
